6f8afa43f1664e75ab28c5424fc54fd1578f58a8
suzy <suzy@users-MacBook-Pro-2.local>
2023-04-05 15:10:51 +0600

    practise4

---
+++ practice4.dart
@@ -0,0 +1,178 @@
+void main(){
+  // int a  = 15;
+  // print(a == 15 ? 'true' : 'false');
+
+
+
+  // int a = 0;
+  // if(a == 0){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // }
+
+  // a = 1;
+  // if(a == 0){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // }
+
+  // a = -3;
+  // if(a == 0){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // }
+
+
+
+
+  // int a = 0;
+  // if(a > 0){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // }
+
+  // a = 1;
+  // if(a > 0){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // }
+
+  // a = -3;
+  // if(a > 0){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // }
+
+
+
+  // int a = 0;
+  // if(a < 0){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // }
+
+  // a = 1;
+  // if(a < 0){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // }
+
+  // a = -3;
+  // if(a < 0){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // }
+
+
+  // int a = 0;
+  // if(a >= 0){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // }
+
+  // a = 1;
+  // if(a >= 0){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // }
+
+  // a = -3;
+  // if(a >= 0){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // }
+
+
+
+
+  //   int a = 0;
+  // if(a <= 0){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // }
+
+  // a = 1;
+  // if(a <= 0){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // }
+
+  // a = -3;
+  // if(a <= 0){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // }
+
+
+
+  // int a = 0;
+  // if(a != 0){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // }
+
+  // a = 1;
+  // if(a != 0){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // }
+
+  // a = -3;
+  // if(a != 0){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // }
+
+
+  // String a = 'test';
+  // if(a == 'тест'){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // } if(a == 'test'){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // }if(a == 3){
+  //   print('true');
+  // }else{
+  //   print('false');
+  // }
+
+
+
+
+  int a = 1;
+  if(a == '1'){
+    print('true');
+  }else{
+    print('false');
+  } if(a == 1){
+    print('true');
+  }else{
+    print('false');
+  }if(a == 3){
+    print('true');
+  }else{
+    print('false');
+  }
+
+}
